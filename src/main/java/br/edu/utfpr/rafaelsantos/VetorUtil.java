dd0af7e50bb8afa664664669559ceecf06514f02
Rafael Santos <rafaelnsantos1@gmail.com>
2018-08-23 20:13:34 -0300

    add teste media

--- src/main/java/br/edu/utfpr/rafaelsantos/VetorUtil.java
+++ src/main/java/br/edu/utfpr/rafaelsantos/VetorUtil.java
@@ -5,4 +5,8 @@ public class VetorUtil {
     public double calcularMediana(double[] vetor) {
         return vetor[vetor.length / 2];
     }
+
+    public double calcularMedia(double[] vetor) {
+        return 9.0;
+    }
 }
--- src/test/java/br/edu/utfpr/rafaelsantos/VetorUtilTest.java
+++ src/test/java/br/edu/utfpr/rafaelsantos/VetorUtilTest.java
@@ -13,17 +13,24 @@ public class VetorUtilTest {
     }
 
     @Test
-    public void medianaTest(){
+    public void medianaTest() {
         double[] vetor = {5.0, 6.0, 9.0};
 
         assertEquals(6.0, vetorUtil.calcularMediana(vetor), 0.1);
     }
 
     @Test
-    public void medianaTest2(){
+    public void medianaTest2() {
         double[] vetor = {5.0, 6.0, 1.0, 2.0, 5.0};
 
         assertEquals(1.0, vetorUtil.calcularMediana(vetor), 0.1);
+    }
+
+    @Test
+    public void mediaTest() {
+        double[] vetor = {8.0, 9.0, 10.0};
+
+        assertEquals(9.0, vetorUtil.calcularMedia(vetor), 0.1);
     }
 
 }
